bad714573b0842e5d2fd9617ccafd5e3b3c0a8be
Azwin Al Mudhoffir <azalmudho@yahoo.com>
2025-11-12 15:27:01 +0100

    Update static_content.js

--- static_content.js
+++ static_content.js
@@ -1,57 +1,55 @@
+// --------------------------------------------------
+// TODO 1.2 and 1.4
+// --------------------------------------------------
+
+// TODO 1.4: Accept the data by declaring a variable that will contain the data like this: 
+// const Introduction = ({your variable name here}) => {...}
 const Introduction = ({ data }) => {
-    // Declare a variable with intro text
-    const introText = "Welcome to the Missing Migrants visualization tool.";
 
-    // Data metrics example
-    const dataMetrics = data
-        ? `This dataset contains ${data.length} records, representing migrant incidents globally.`
-        : "Loading data metrics...";
+    // TODO 1.2: Declare a variable which contains your intro text by writing: const introText = "your text here";
+    const count = data ? data.length : 0;
+    const totalMissing = data ? d3.sum(data, d => d['Total Dead and Missing']) : 0;
+    const dateExtent = data ? d3.extent(data, d => d['Reported Date']) : [null, null];
+    const fmtDate = d3.timeFormat('%b %d, %Y');
+    const introText = data
+        ? ('This dataset contains ' + count + ' incidents with a total of ' + totalMissing + 
+           ' dead or missing. Reporting dates range from ' + (dateExtent[0] ? fmtDate(dateExtent[0]) : 'N/A') + 
+           ' to ' + (dateExtent[1] ? fmtDate(dateExtent[1]) : 'N/A') + '.')
+        : 'Loading dataset metrics…';
 
+    // This returns the html elements which should be created by this component. A react fragment is required because
+    // multiple elements are used.
     return (
         <>
-            {/* Title Section */}
-            <div className="introTitle">
-                Description
-                <br />
-            </div>
-
-            {/* Intro Section */}
-            <div className="intro">
-                {introText}
-                <br />
-                {dataMetrics}
-            </div>
+            <div className="introTitle">Description<br/></div>
+            <div className="intro">{introText}</div>
         </>
-    );
+    )
 };
 
+// --------------------------------------------------
+// TODO 1.5 (World Sphere and Graticule)
+// --------------------------------------------------
 
-const WorldGraticule = () => {
-    const width = 960;
-    const height = 500;
-    // Define a projection
-    const projection = d3.geoNaturalEarth1().fitSize([width, height], { type: "Sphere" });
-
-    // Define a path generator
-    const path = d3.geoPath(projection);
-
-    // Generate the longitude/latitude grid lines
-    const graticule = d3.geoGraticule();
-
-    // Memoize sphere and graticules for better performance
-    const sphere = React.useMemo(() => ({ type: "Sphere" }), []);
-    const graticuleLines = React.useMemo(() => graticule(), [graticule]);
-
-    return (
-        <g className="worldGraticule">
-            {/* Sphere */}
-            <path d={path(sphere)} fill={"lightblue"}/>
-
-            {/* Graticules */}
-            <path d={path(graticuleLines)} fill="none" stroke={"#ececec"}/>
-        </g>
-    );
-};
+const WorldGraticule = () => (
+    // TODO 4.2: Memoization for sphere and graticules
+    // TODO 1.5: add className worldGraticule to style with information from css to the g tag
+    <g className="worldGraticule">
+        <>
+            {(() => {
+                const projection = d3.geoNaturalEarth1();
+                const path = d3.geoPath(projection);
+                const graticule = d3.geoGraticule();
+                return (
+                    <>
+                        <path className="sphere" d={path({ type: 'Sphere' })} />
+                        <path className="graticules" d={path(graticule())} />
+                    </>
+                );
+            })()}
+        </>
+    </g>
+);
 
 const Countries = ({ worldAtlas: { land, interiors } }) => {
     const width = 960;
